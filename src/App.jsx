b48a62cf08798c31bbd08db0cea92ced12b5ba3d
Markus Jarvis <markus.h.jarvis@gmail.com>
2025-02-24 16:34:18 -0600

    Add PageNotFound page and import to router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import "./App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Dashboard from "./components/pages/Dashboard"
+import Appliances from "./components/pages/Appliances"
+import PageNotFound from "./components/pages/PageNotFound"
+import Forum from "./components/pages/Forum"
+import Login from "./components/pages/Login"
+import Maintenance from "./components/pages/Maintenance"
+import Schedule from "./components/pages/Schedule"
+import Weather from "./components/pages/Weather"
 
 export default function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Dashboard />} />
-				<Route path="Appliances" element={<div>Appliances</div>} />
-				<Route path="/forums" element={<div>Forums</div>} />
-				<Route path="/login" element={<div>Login</div>} />
-				<Route path="/maintenance" element={<div>Maintenance</div>} />
-				<Route path="/schedule" element={<div>Schedule</div>} />
-				<Route path="/weather" element={<div>Weather</div>} />
+				<Route path="/appliances" element={<Appliances />} />
+				<Route path="/forums" element={<Forum />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/maintenance" element={<Maintenance />} />
+				<Route path="/schedule" element={<Schedule />} />
+				<Route path="/weather" element={<Weather />} />
+				<Route path="*" element={<PageNotFound />} />
 			</Routes>
 		</BrowserRouter>
 	)
