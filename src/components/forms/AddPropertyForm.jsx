ad9c7979683a42fd55bf6e1897585ad50cc2294f
Markus Jarvis <markus.h.jarvis@gmail.com>
2025-02-24 20:49:59 -0600

    Add placeholder for new properties form

---
+++ src/components/forms/AddPropertyForm.jsx
@@ -0,0 +1,5 @@
+import styles from "./AddPropertyForm.module.css"
+
+export default function AddPropertyForm() {
+	return <h1>Property Form Placeholder</h1>
+}
--- src/pages/PropertyDetails.jsx
+++ src/pages/PropertyDetails.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react"
+import styles from "./PropertyDetails.module.css"
+import AddPropertyForm from "../components/forms/AddPropertyForm"
 import MainWithSidebar from "../templates/MainWithSidebar"
 
 export default function PropertyDetails() {
+	const [showForm, setShowForm] = useState(false)
+
+	const handleClick = () => {
+		setShowForm((prev) => !prev)
+	}
+
 	return (
 		<MainWithSidebar>
-			<h1>Property Details</h1>
+			<button className={styles.addPropertyButton} onClick={handleClick}>
+				Add New Property
+			</button>
+			{showForm && <AddPropertyForm />}
 		</MainWithSidebar>
 	)
 }
