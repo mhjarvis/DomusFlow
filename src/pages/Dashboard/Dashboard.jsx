94adec234c1c1ece4696643748d24e47dd4ac2d5
Markus Jarvis <markus.h.jarvis@gmail.com>
2025-02-25 21:22:53 -0600

    Add PropertyOverview component

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,29 @@
 import styles from "./Dashboard.module.css"
 import MainWithSidebar from "../../templates/MainWithSidebar"
 import PropertyOverview from "./PropertyOverview"
+import { useEffect, useState } from "react"
+
 export default function Dashboard() {
+	const [property, setProperty] = useState(null)
+
+	// import and set state using first property in json file
+	useEffect(() => {
+		async function fetchProperty() {
+			const data = await import("../../data/properties.json")
+			setProperty(data.properties[0])
+		}
+		fetchProperty()
+	}, [])
+
+	// display loading message while fetching data
+	if (!property) {
+		return <div>Loading...</div>
+	}
+
 	return (
 		<MainWithSidebar>
 			<div className={styles.dashboard}>
-				<PropertyOverview />
+				<PropertyOverview data={property} />
 			</div>
 		</MainWithSidebar>
 	)
